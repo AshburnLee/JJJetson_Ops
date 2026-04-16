71f268f8be63eaeb257699a7e430426cf7abee50
AshburnLee <1578034415@qq.com>
2026-04-16 13:27:47 +0800

    add device info & peak throughput

---
+++ tools/show_device_info.cu
@@ -0,0 +1,21 @@
+// 仅用于在 host 侧打印当前 GPU 信息
+#include "device_hw_info.cuh"
+
+#include <cstdlib>
+
+int main(int argc, char** argv) {
+    int device_id = 0;
+    if (argc > 1) {
+        device_id = std::atoi(argv[1]);
+    }
+
+    cudaError_t st = cudaSetDevice(device_id);
+    if (st != cudaSuccess) {
+        std::fprintf(stderr, "cudaSetDevice(%d) failed: %s\n", device_id, cudaGetErrorString(st));
+        return 1;
+    }
+
+    const DeviceHwInfo h = query_device_hw_info(device_id);
+    fprint_device_hw_info(stdout, h);
+    return 0;
+}
